b33dbe1c6e43be1eef0a3c0549173dc9922c0b98
Jefferson-Marcelo <jeffersonmarcelo_os@hotmail.com>
2020-07-24 21:26:33 -0300

    inicio para inserir o código JS no browser

--- Pages/js/script.js
+++ Pages/js/script.js
@@ -1,3 +1,16 @@
+function gerarDias(start,stop){
+    day=[]
+    for(let dias=start; dias< stop+1; dias++){
+        day.push(dias)
+    }
+    return day
+}
+
+function bissexto(ano){
+    (ano%4 === 0 || ano%400 === 0)?mySelectDay.add(new Option(gerarDias(1,29)),null):mySelectDay.add(new Option(gerarDias(1,28)),null)
+    return day
+}
+
       //Ano
      let mySelectYear=document.getElementById("year"), 
      year = new Date().getFullYear();
@@ -8,47 +21,23 @@
                      }(121);
     
         
-                    
+let mySelectMonth = document.getElementById('month')             
+let mySelectDay = document.getElementById('day')
 
-   //     function validacaoData(dia,mes, ano){            
-   //         let getDia = document.getElementById("day")
-   //     }
-   //
-        function selectDayTratamento (mes,dia, ano){            
-            if(mes == 'Janeiro' || 'Março' ||'Maio' || 'Julho' ||'Agosto' ||'Outubro' ||'Dezembro'){
-                for (let dias=1; dias<32; dias++){
-                    dia.append(dias)
-                } 
+        function selectDayTratamento (mes,year){      
+            const meses = ['Janeiro', 'Março', 'Maio', 'Julho', 'Agosto', 'Outubro','Dezembro']
+            for (let i in meses){
+                if (mes === meses[i]){
+                    mySelectDay.add(new Option(gerarDias(1,31)),null)
+                    break
+                }
+                else mySelectDay.add(new Option(gerarDias(1,30)),null)
             }
-            else if(mes = 'Abril'||'Junho' || 'Setembro' || 'Novembro'){
-                for (let dias=1; dias<31; dias++){
-                    dia.append(dias)
-                } 
-            }
-            
-            else if(ano)
-                function bissexto(ano){
-                    if(ano%4 === 0 || ano%400 === 0){
-                        let dia = 29
-                    }                               
-                }      
-                return  console.log()
+            if(mes === 'Fevereiro'){
+              bissexto(year)        
+            }    
+            return console.log(mes, day)
         }
         
-        
-
-    //Mes
-  //   let mySelectMonth=document.getElementById("month")
-  //          
-  //   month = new Date().me[mySelectMonth.getMonth()];    
-  //   
-  //   let me = ['Janeiro', 'Fevereiro', 'Março',  'Abril','Maio', 'Junho', 'Julho', 
-  //  'Agosto','Setembro' ,'Outubro' ,'Novembro' ,'Dezembro']
-
-    //   let meses = {Janeiro: 0 ,Fevereiro :1 ,Março:2 ,//Abril:3 ,Maio: 4 ,Junho: 5 ,Julho: 6 ,Agosto: 7 ,//Setembro: 8,Outubro: 9, Novembro: 10, Dezembro: 11}
-    //  
-    //  function selecionarMeses(array){
-    //    console.log(array) 
-    //  }
-//
-    //  selecionarMeses(me)
+        selectDayTratamento("Fevereiro", 2016)
+                    
