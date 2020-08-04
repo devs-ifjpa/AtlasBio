b2942098b47c1562783b3cf8abe587f701ed6145
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2020-08-04 01:18:10 -0300

    tentando scriptar

--- Pages/js/script.js
+++ Pages/js/script.js
@@ -1,14 +1,12 @@
-$(document).ready(function(){
-    $(".hamburguer").click(function(){
-        $(this).toggleClass("active")
-        $(".menu").toggleClass("active")
-    });
-});
+// $(document).ready(function(){
+//     $(".hamburguer").click(function(){
+//         $(this).toggleClass("active")
+//         $(".menu").toggleClass("active")
+//     });
+// });
 
 function Desvasado(doc){
     let link = doc.outerHTML.split(" ")[2].split("=")[1];
-    console.log(link);
-    console.log('"../img/footer/Botão-Opções-Vazado.png"');
     if(link == '"../img/footer/Botão-Opções-Vazado.png"'){
         doc.src = '../img/footer/menu_pintado.svg'
     }else{
@@ -17,47 +15,50 @@ function Desvasado(doc){
 }
 
 function gerarDias(start,stop){
+    let mySelectDay = document.getElementById('day')
     day=[]
     for(let dias=start; dias< stop+1; dias++){
         day.push(dias)
     }
-    return day
+    day.forEach(e => {
+        mySelectDay.add(new Option(e,null))
+    });
 }
 
 function bissexto(ano){
-    (ano%4 === 0 || ano%400 === 0)?mySelectDay.add(new Option(gerarDias(1,29)),null):mySelectDay.add(new Option(gerarDias(1,28)),null)
+    (ano%4 === 0 || ano%400 === 0)?gerarDias(1,29):gerarDias(1,28)
     return day
 }
 
-    if(document.getElementById("year") != undefined){
+if(document.getElementById("year") != undefined){
         //Ano
-        let mySelectYear = document.getElementById("year"), 
-        year = new Date().getFullYear();
-        let gen = function(max){
-            do{
-                mySelectYear.add(new Option(year--,max--),null);
-            }while(max>0);
-        }(121);
-    }
-    
-        
-let mySelectMonth = document.getElementById('month')             
-let mySelectDay = document.getElementById('day')
+    let mySelectYear = document.getElementById("year")
+    year = new Date().getFullYear();
+    let gen = function(max){
+        do{
+            mySelectYear.add(new Option(year--,max--),null);
+        }while(max>0);
+    }(121);
+}
 
-        function selectDayTratamento (mes,year){      
-            const meses = ['Janeiro', 'Março', 'Maio', 'Julho', 'Agosto', 'Outubro','Dezembro']
-            for (let i in meses){
-                if (mes === meses[i]){
-                    mySelectDay.add(new Option(gerarDias(1,31)),null)
-                    break
-                }
-                else mySelectDay.add(new Option(gerarDias(1,30)),null)
-            }
-            if(mes === 'Fevereiro'){
-              bissexto(year)        
-            }    
-            return console.log(mes, day)
+function selectDayTratamento (mes,year){
+    const meses = ['Jan', 'Mar', 'Mai', 'Jul', 'Ago', 'Out','Dez']
+    for (let i in meses){
+        if (mes === meses[i]){
+            gerarDias(1,31)
+            break
         }
-        
-        selectDayTratamento("Fevereiro", 2016)
-                    
+        else gerarDias(1,30)
+    }
+    if(mes === 'Fev'){
+        bissexto(year)
+    }
+    return console.log(mes, day)
+}
+
+let mySelectMonth = document.querySelector('[wm-month] option')
+let mySelectYear = document.getElementById("year")
+
+selectDayTratamento(mySelectMonth.innerHTML, mySelectYear.innerText)
+
+addEventListener('change',selectDayTratamento(mySelectMonth.innerHTML, mySelectYear.innerText))
